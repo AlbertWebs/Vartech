e88dd4b5500c77db41fffd8cf4d265729a259757
AlbertWebs <albertmuhatia@gmail.com>
2022-07-28 11:01:51 +0300

    Logo

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -105,7 +105,7 @@
     <nav class="navbar navbar-expand-lg fixed-top white-bg">
         <div class="container">
             <a class="navbar-brand" href="index-2.html">
-                <img src="img/logo-color.png" alt="logo" class="img-fluid"/>
+                <img width="280" src="{{asset('theme/img/website logos-4.png')}}" alt="Vartech East Africa" class="img-fluid"/>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
